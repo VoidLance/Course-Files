13ed4c1eb5431fe9b1685321f93207d790f31f76
Alistair Sweeting <alistair.m.sweeting@gmail.com>
2025-11-15 19:52:51 +0000

    did some stuff included in the lessons

--- JavaScript/FirstCode/script.js
+++ JavaScript/FirstCode/script.js
@@ -2,3 +2,23 @@ function replace() {
   var paragraph = document.getElementById("demo");
   paragraph.textContent = "New Text!";
 }
+
+// Using age variables and functions from class, but also adding a function to change age to minor for demonstration using console
+
+var myAge = 25;
+function makeMinor() {
+  myAge = 15;
+}
+function greet() {
+if (myAge >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
+}
+
+window.onload = function() {
+  console.log("Page is fully loaded");
+  this.alert("Welcome to the page!");
+  greet();
+} 
