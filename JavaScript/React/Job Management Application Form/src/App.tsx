94f74ae3ab61fc9ac0f6074d328fcbc26d08d1b6
VoidLance <alistair.m.sweeting@gmail.com>
2026-02-07 21:26:24 +0000

    Created Job Management Application Form
    as usual, with all bonus challenges met

---
+++ JavaScript/React/Job Management Application Form/src/App.tsx
@@ -0,0 +1,13 @@
+import { JobForm } from "./components/JobForm";
+import "./index.css";
+
+
+export function App() {
+  return (
+    <div className="app">
+    <JobForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ JavaScript/React/Job Management Application Form/src/components/JobForm.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+export const JobForm = () => {
+  const [jobs, setJobs] = useState([{id:1, name:"Read Emails", status:"running"}, {id: 2, name:"Web Parsing", status:"completed"}, {id:3, name:"Send Emails", status:"stopped"}])
+
+ const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const newJob = {
+        id: jobs.length > 0 ? jobs[jobs.length - 1].id + 1 : 1, // Increment the last item's id or start at 1
+        name: formData.get('name') as string,
+        status: formData.get('status') as string,
+    };
+
+    setJobs((prevJobs) => [...prevJobs, newJob]);
+    (event.target as HTMLFormElement).reset();
+};
+
+
+  return (
+    <div className="form-header">
+    <form onSubmit={handleSubmit}>
+    <input type="text" className="bot-input" name='name' placeholder="Enter the job to add" required />
+    <div className="form-details">
+    <div className="bottom-line">
+    {jobs.map((id) => (<button key={id.id} type="button" onClick={()=>console.log(id.status)}>{id.name}</button>))}
+    </div>
+    <select className="job-status" name='job-status' required>
+    <option value="running">Running</option>
+    <option value="completed">Completed</option>
+    <option value="stopped">Stopped</option>
+    </select>
+    </div>
+    <button type="submit" className="submit-data">Add Job</button>
+    </form>
+    </div>
+  );
+};
