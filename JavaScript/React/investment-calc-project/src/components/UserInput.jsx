92c29976222ac51389544e87be964bdd98afdb10
VoidLance <alistair.m.sweeting@gmail.com>
2026-02-19 15:51:14 +0000

    Improve user input restrictions to be more accurate to real use

--- JavaScript/React/investment-calc-project/src/components/UserInput.jsx
+++ JavaScript/React/investment-calc-project/src/components/UserInput.jsx
@@ -12,12 +12,12 @@ const UserInput = ({callUserInput, inputval }) => {
     <input type="number" id="annualInvestment" value={inputval.annInvestment} onChange={(e)=> callUserInput('annInvestment', e.target.value)} required min="0"/>
     </p>
     <p className="input-group">
-    <label htmlFor="expectedReturn">Expected Return ($)</label>
-    <input type="number" id="expectedReturn" value={inputval.returnInv} onChange={(e)=> callUserInput('returnInv', e.target.value)} required min="0"/>
+    <label htmlFor="expectedReturn">Expected Return (%)</label>
+    <input type="number" id="expectedReturn" value={inputval.returnInv} onChange={(e)=> callUserInput('returnInv', e.target.value)} required/>
     </p>
     <p className="input-group">
     <label htmlFor="yearlyInvestment">Duration (Years)</label>
-    <input id="yearlyInvestment" type="number" value={inputval.yearInv} onChange={(e)=> callUserInput('yearInv', e.target.value)} required min="0"/>
+    <input id="yearlyInvestment" type="number" value={inputval.yearInv} onChange={(e)=> callUserInput('yearInv', e.target.value)} required min="1"/>
     </p>
     </section>
   )
