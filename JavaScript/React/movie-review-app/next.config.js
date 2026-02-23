2c3970bebdccbc656335ec843f05018e0d57da0b
VoidLance <alistair.m.sweeting@gmail.com>
2026-02-23 13:00:17 +0000

    Refactor Next.js configuration to remove unused settings and streamline image handling

--- JavaScript/React/movie-review-app/next.config.js
+++ JavaScript/React/movie-review-app/next.config.js
@@ -5,18 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
   },
 };
 
-export default config;
+module.exports = nextConfig;
