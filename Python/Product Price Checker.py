a038eb2128cc6e21dd5eb05a3993044312448c55
VoidLance <alistair.m.sweeting@gmail.com>
2026-03-10 09:22:03 +0000

    Added comments to price checker app

--- Python/Product Price Checker.py
+++ Python/Product Price Checker.py
@@ -1,9 +1,13 @@
-hoop = 9.45
-barista = 264.65
+hoop = 9.45 # create variable to represent the Hoop coffee filter's price
+barista = 264.65 # Create variable to represent the standard Barista espresso machine's price
 
-if hoop > barista:
-    print("Hoop is more expensive than barista")
-elif hoop < barista:
-    print("Hoop is less expensive than barista")
-else:
-    print("Both products are the same price")
+if hoop > barista: # if the value of the hoop variable is higher than the value of the barista variable
+    print("Hoop is more expensive than barista") # print that to the console
+elif hoop < barista: # otherwise if the value of the hoop variable is less than the value of the barista variable
+    print("Hoop is less expensive than barista") # print that to the console
+else: # In any other case (meaning if hoop is neither less than or greater than barista)
+    print("Both products are the same price") # print that to the console
+
+
+# I hope these comments are appropriate, I'm not a fan of commenting things that are obvious, which inevitably means
+# that I often don't comment anything and subsequently forget what it does
