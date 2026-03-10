175268d485b76bd926d551c8c786298b4bbc1dba
VoidLance <alistair.m.sweeting@gmail.com>
2026-03-10 11:11:58 +0000

    Updated Personal Information Card.py to use OOP instead so that it is more scalable

--- Python/Personal Information Card.py
+++ Python/Personal Information Card.py
@@ -1,16 +1,27 @@
+# Define class to use for multiple instances as a more scalable solution
+class Person:
+    def __init__(self,name,age,city,hobby):
+        self.name = name
+        self.age = age
+        self.city = city
+        self.hobby = hobby
+
+# Define new Person with the attributes used in the previous solution
+alistair = Person("Alistair", 29, "Bristol", "Coding")
+
 # Declare all variables
-name=str("Alistair")
-age=int(29)
-city=str("Bristol")
-hobby=str("Coding")
+# name=str("Alistair")
+# age=int(29)
+# city=str("Bristol")
+# hobby=str("Coding")
 
 # Print card information with decorative bars inspired by example
 print("====================================================================")
 print("===========Information Card=========================================")
 print("====================================================================")
-print("             Name:    ",name)
-print("             Age:     ",age)
-print("             City:    ",city)
-print("             Hobby:   ",hobby)
+print("             Name:    ",alistair.name)
+print("             Age:     ",alistair.age)
+print("             City:    ",alistair.city)
+print("             Hobby:   ",alistair.hobby)
 print("====================================================================")
 print("====================================================================")
