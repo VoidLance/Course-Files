e2171e0911f6cd0a7d6b0e34c89d5227bcc4a53a
VoidLance <alistair.m.sweeting@gmail.com>
2026-03-10 11:02:05 +0000

    Created Personal Information Card.py

---
+++ Python/Personal Information Card.py
@@ -0,0 +1,16 @@
+# Declare all variables
+name=str("Alistair")
+age=int(29)
+city=str("Bristol")
+hobby=str("Coding")
+
+# Print card information with decorative bars inspired by example
+print("====================================================================")
+print("===========Information Card=========================================")
+print("====================================================================")
+print("             Name:    ",name)
+print("             Age:     ",age)
+print("             City:    ",city)
+print("             Hobby:   ",hobby)
+print("====================================================================")
+print("====================================================================")
