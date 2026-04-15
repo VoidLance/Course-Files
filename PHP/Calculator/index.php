06e7ea960f08c73f46d5f71902c72f4573637ada
VoidLance <alistair.m.sweeting@gmail.com>
2026-04-15 16:25:23 +0100

    Add PHP calculator functionality and update document title

--- PHP/Calculator/index.php
+++ PHP/Calculator/index.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Calculator</title>
 </head>
 <body>
-    
+    <?php
+    // A simple PHP calculator that uses a function to add two variables with numerical values, and then output the sum of these two variables in an HTML format
+    function addNumbers() {
+        $num1 = 10;
+        $num2 = 25;
+        $sum = $num1 + $num2;
+        echo "<H2>The sum of ".$num1." and ".$num2." is ".$sum.".</H2>";
+    }
+    addNumbers();
+    ?>
 </body>
 </html>
